db437ab817263ba816db4432874c4308901039a6
Kris <kris_s2@outlook.com>
2020-12-27 21:01:25 -0800

    KS-Updating project to .net 4.8 and Quartz library v.3.2.3

---
+++ ConsAppQuartz/MyApp.cs
@@ -0,0 +1,37 @@
+﻿using Quartz;
+using Quartz.Impl;
+using JobLibrary;
+using static System.Console;
+
+namespace ConsAppQuartz
+{
+    class MyApp
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            ReadLine();
+        }
+
+        public static async void Run()
+        {
+            //Construct scheduler factory;
+            ISchedulerFactory schFactory = new StdSchedulerFactory();
+
+            IScheduler scheduler = await schFactory.GetScheduler(new System.Threading.CancellationToken());
+            await scheduler.Start();
+
+            //Job
+            IJobDetail jobDetail = JobBuilder.Create<SimplyJob>().WithIdentity("job1", "group1").Build();
+
+            //Trigger
+            ITrigger jobTrigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever())
+                .Build();
+
+            //schedule
+            await scheduler.ScheduleJob(jobDetail, jobTrigger);
+        }
+    }
+}
---
+++ JobLibrary/SimplyJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace JobLibrary
+{
+    public class SimplyJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Task.Run(()=> 
+            {
+                Console.WriteLine("Hello, JOb executed");
+            }); 
+        }
+    }
+}
